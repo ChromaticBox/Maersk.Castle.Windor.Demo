7db1940145fd8c24d667bb158405f63efe09dc15
Rik Svendsen Rose <rsr@gait.dk>
2021-02-26 09:10:56 +0100

    As services registration and debugging

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,6 +1,12 @@
+using System.Linq;
 using System.Reflection;
 using Api.Basic.Extensions;
+using Api.Basic.Impl;
+using Api.Basic.Impl.Interfaces;
+using Castle.Core;
+using Castle.MicroKernel;
 using Castle.Windsor;
+using Castle.Windsor.Diagnostics;
 using Castle.Windsor.Installer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,7 +37,11 @@ namespace Api.Basic
             //Using .NET lifestyle semantics, any service using the Castle Windsor transient lifestyle, and directly injected into the controller,
             //will not be released. To allow the standard Windsor behavior for services directly injected in the controllers, you will have to allow castle Windsor
             //to resolve and release the controllers. To do that add AddControllerAsServices to the call to ConfigureServices in your Startup class:
-                .AddControllersAsServices(); 
+                .AddControllersAsServices();
+            
+            //You can register with MS implementation, or castle way. up to you.
+            //services.AddTransient<IFoo, Foo>();
+            //services.AddTransient<IBar, Bar>();
         }
 
 #region InstallInConfigureContainer Step2-Recommened
@@ -39,13 +49,17 @@ namespace Api.Basic
         //Step 2
         public void ConfigureContainer (IWindsorContainer container)
         {
+            GetRegisteredComponentsFromCastleWindsorDiagnostics(container);
+            
             var logger = container.Resolve<ILogger<Startup>>();
             logger.LogInformation("Startup ConfigureContainer");
 
-            container.Install(FromAssembly.InThisApplication(Assembly.GetEntryAssembly()));
+            //container.Install(FromAssembly.InThisApplication(Assembly.GetEntryAssembly()));
             //container.Install(new AutomaticInstaller(), new ManualInstaller());
+            //GetRegisteredComponentsFromCastleWindsorDiagnostics(container);
         }
-#endregion
+
+        #endregion
 
         //Step 3
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -68,6 +82,16 @@ namespace Api.Basic
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void  GetRegisteredComponentsFromCastleWindsorDiagnostics(IWindsorContainer container)
+        {
+            var host = (IDiagnosticsHost) container.Kernel.GetSubSystem(SubSystemConstants.DiagnosticsKey);
+            var diagnostics = host.GetDiagnostic<IAllComponentsDiagnostic>();
+            var handlers = diagnostics.Inspect();
+            if(!handlers?.Any() ?? true) return;
+
+            var components = handlers.Select(x => x.ComponentModel.ComponentName).ToArray();
         }
     }
 }
